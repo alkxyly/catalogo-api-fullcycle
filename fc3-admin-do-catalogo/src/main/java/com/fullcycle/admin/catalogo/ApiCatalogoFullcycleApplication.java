15a115a080165b2ae341392f11a0f88805993b9a
alkxyly <alkxyly@hotmail.com>
2022-10-18 15:33:52 -0300

    first commit

---
+++ fc3-admin-do-catalogo/src/main/java/com/fullcycle/admin/catalogo/ApiCatalogoFullcycleApplication.java
@@ -0,0 +1,13 @@
+package com.fullcycle.admin.catalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogoFullcycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCatalogoFullcycleApplication.class, args);
+	}
+
+}
---
+++ fc3-admin-do-catalogo/src/test/java/com/fullcycle/admin/catalogo/fc3admindocatalogo/ApiCatalogoFullcycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcycle.admin.catalogo.fc3admindocatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCatalogoFullcycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
