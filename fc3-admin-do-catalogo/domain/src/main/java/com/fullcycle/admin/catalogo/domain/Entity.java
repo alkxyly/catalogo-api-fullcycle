2579818331ed7ab1cdfbf9b491e03bb250af32bf
alkxyly <alkxyly@hotmail.com>
2022-10-18 23:54:30 -0300

    feat: implementando as classes genericas do DDD

---
+++ fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/Entity.java
@@ -0,0 +1,27 @@
+package com.fullcycle.admin.catalogo.domain;
+
+import java.util.Objects;
+
+public abstract class Entity<ID extends Identifier> {
+    protected final ID id;
+    protected Entity(final ID id) {
+        Objects.requireNonNull(id, "'id' should not be null");
+        this.id = id;
+    }
+    public ID getID() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Entity<?> entity = (Entity<?>) o;
+        return Objects.equals(id, entity.getID());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
