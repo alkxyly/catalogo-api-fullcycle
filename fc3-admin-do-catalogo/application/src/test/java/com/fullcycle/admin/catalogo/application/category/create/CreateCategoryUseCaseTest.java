63a2c89db78dbb0ae60f2b0c0a51793b6f352fd3
alkxyly <alkxyly@hotmail.com>
2022-10-26 23:23:00 -0300

    test: implementando o teste CreateCategoryUseCaseTest quando o nome é inválido

--- fc3-admin-do-catalogo/application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ fc3-admin-do-catalogo/application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,20 +1,15 @@
 package com.fullcycle.admin.catalogo.application.category.create;
 
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
-import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-
+import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Objects;
-import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.*;
 
 class CreateCategoryUseCaseTest {
 
@@ -49,12 +44,32 @@ class CreateCategoryUseCaseTest {
                     return Objects.equals(expectedName, aCategory.getName())
                             && Objects.equals(expectedDescription, aCategory.getDescription())
                             && Objects.equals(expectedIsActive, aCategory.isActive())
-                            && nonNull(aCategory.getID())
-                            && nonNull(aCategory.getCreatedAt())
-                            && nonNull(aCategory.getUpdatedAt())
-                            && isNull(aCategory.getDeletedAt());
+                            && Objects.nonNull(aCategory.getID())
+                            && Objects.nonNull(aCategory.getCreatedAt())
+                            && Objects.nonNull(aCategory.getUpdatedAt())
+                            && Objects.isNull(aCategory.getDeletedAt());
                     }
                 ));
+    }
 
+    @Test
+    void givenAnInValidName_whenCallCreateCategory_thenShouldReturnDomainException(){
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expetedMessageException = "'name' should not be null";
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var gateway =  Mockito.mock(CategoryGateway.class);
+
+        Mockito.when(gateway.create(any()))
+                .thenAnswer(returnsFirstArg());
+
+        final var useCase = new DefaultCreateCategoryUseCase(gateway);
+
+        final var actualException =  assertThrows( DomainException.class, () -> useCase.execute(aCommand));
+
+        assertEquals(expetedMessageException, actualException.getMessage());
+
+        Mockito.verify(gateway, times(0)).create(any());
     }
 }
