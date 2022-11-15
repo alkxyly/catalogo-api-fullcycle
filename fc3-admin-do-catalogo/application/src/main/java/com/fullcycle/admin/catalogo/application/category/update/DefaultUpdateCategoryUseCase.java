d68d0717113f0d18fe83be0f39f6d764edb30622
alkxyly <alkxyly@hotmail.com>
2022-11-15 20:30:44 -0300

    feat: cobrindo com demais cenários de teste do caso de uso de atualização de categoria

--- fc3-admin-do-catalogo/application/src/main/java/com/fullcycle/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
+++ fc3-admin-do-catalogo/application/src/main/java/com/fullcycle/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
@@ -10,7 +10,8 @@ import io.vavr.API;
 import io.vavr.control.Either;
 
 import java.util.function.Supplier;
-
+import static io.vavr.API.Left;
+import static io.vavr.API.Try;
 public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase{
 
     private final CategoryGateway categoryGateway;
@@ -18,30 +19,33 @@ public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase{
     public DefaultUpdateCategoryUseCase(final CategoryGateway categoryGateway){
         this.categoryGateway = categoryGateway;
     }
-
     @Override
     public Either<Notification, UpdateCategoryOutput> execute(final UpdateCategoryCommand aCommand) {
         final var anId = CategoryID.from(aCommand.id());
+        final var aName = aCommand.name();
+        final var aDescription = aCommand.description();
+        final var isActive = aCommand.isActive();
 
-        final var aCategory =  this.categoryGateway.findById(anId)
+        final var aCategory = this.categoryGateway.findById(anId)
                 .orElseThrow(notFound(anId));
 
         final var notification = Notification.create();
-
         aCategory
-                .update(aCommand.name(), aCommand.description(), aCommand.isActive())
+                .update(aName, aDescription, isActive)
                 .validate(notification);
 
-        return notification.hasError() ? API.Left(notification): update(aCategory);
+        return notification.hasError() ? Left(notification) : update(aCategory);
     }
 
-    private Either<Notification, UpdateCategoryOutput > update(Category aCategory){
-        return API.Try(() -> this.categoryGateway.update(aCategory))
+    private Either<Notification, UpdateCategoryOutput> update(final Category aCategory) {
+        return Try(() -> this.categoryGateway.update(aCategory))
                 .toEither()
                 .bimap(Notification::create, UpdateCategoryOutput::from);
     }
-    private static Supplier<DomainException> notFound(CategoryID anId) {
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
         return () -> DomainException.with(
-                new Error("Category with ID %s was not found".formatted(anId.getValue())));
+                new Error("Category with ID %s was not found".formatted(anId.getValue()))
+        );
     }
 }
