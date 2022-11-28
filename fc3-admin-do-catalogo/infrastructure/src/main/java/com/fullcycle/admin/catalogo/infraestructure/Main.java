6a2ca24271cf5a2a9df654cd0a21186a24f8f5b5
alkxyly <alkxyly@hotmail.com>
2022-11-27 22:06:21 -0300

    config: configuração inicial

--- fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infraestructure/Main.java
+++ fc3-admin-do-catalogo/infrastructure/src/main/java/com/fullcycle/admin/catalogo/infraestructure/Main.java
@@ -3,10 +3,12 @@ package com.fullcycle.admin.catalogo.infraestructure;
 import com.fullcycle.admin.catalogo.infraestructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME,"development");
         SpringApplication.run(WebServerConfig.class,args);
     }
 }
